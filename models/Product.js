4960011f574831bad9ec0c1c98560eeec7258651
benos <benantonn@gmail.com>
2023-06-29 17:05:49 -0500

    complete model associations in models>index.js

--- models/Product.js
+++ models/Product.js
@@ -44,7 +44,6 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'Category', // name of Target model
         key: 'id', // key in Target model that we're referencing
